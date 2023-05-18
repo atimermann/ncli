dec08901d89dc3166b3779b646d100e9d4866c07
André Timermann <andre@timermann.com.br>
2023-05-18 17:48:18 -0300

    validação de execução inválida

--- src/template/project/src/main.mjs
+++ src/template/project/src/main.mjs
@@ -8,10 +8,14 @@
  *
  */
 import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { pathToFileURL, fileURLToPath } from 'url'
 import { Application } from '@agtm/node-framework'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const {{NAME}} = new Application(__dirname, '{{NAME}}')
 
 export default {{NAME}}
+
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+  throw new Error('This module should not be executed directly, use \'run.mjs\' instead.')
+}
