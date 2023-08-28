15620a266f0d590643eecd5613ee3f231d5f0d7f
André Timermann <andre@timermann.com.br>
2023-08-27 22:51:44 -0300

    Melhoria no template  project, main.mjs, simplificação

--- src/template/project/src/main.mjs
+++ src/template/project/src/main.mjs
@@ -7,15 +7,11 @@
  * {{DESCRIPTION}}
  *
  */
-import { dirname } from 'path'
-import { pathToFileURL, fileURLToPath } from 'url'
-import { Application } from '@agtm/node-framework'
+import { __dirname } from '@agtm/util'
+import { Application, checkExecution } from '@agtm/node-framework'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const {{NAME}} = new Application(__dirname, '{{NAME}}')
+checkExecution(import.meta.url)
+
+const {{NAME}} = new Application(__dirname(import.meta.url), '{{NAME}}')
 
 export default {{NAME}}
-
-if (import.meta.url === pathToFileURL(process.argv[1]).href) {
-  throw new Error('This module should not be executed directly, use \'run.mjs\' instead.')
-}
