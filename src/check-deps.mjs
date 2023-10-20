f9e82fcb02ea01b2ef2e4486817b1ad41fca5797
André Timermann <andre@timermann.com.br>
2023-10-20 16:36:31 -0300

    - implementação check-deps - atualização eslint

--- src/check-deps.mjs
+++ src/check-deps.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
+import { access, constants, stat, readdir } from 'node:fs/promises'
 import { join } from 'path'
 import { simpleGit } from 'simple-git'
 import { loadJson } from '@agtm/util'
@@ -26,7 +26,6 @@ async function checkGitStatus (modulePath) {
 // Função para verificar se um módulo é publicável
 async function isModulePublishable (modulePath) {
   try {
-
     const git = simpleGit(modulePath)
 
     const latestReleaseTag = (await git.tags()).latest
@@ -40,7 +39,6 @@ async function isModulePublishable (modulePath) {
       publishable: commits.total > 0 ? PUBLISHABLE : NOT_PUBLISHABLE,
       latestReleaseTag
     }
-
   } catch (error) {
     console.error(`Erro ao verificar a publicabilidade do módulo: ${error}`)
     return false // Trate erros retornando false
@@ -48,22 +46,19 @@ async function isModulePublishable (modulePath) {
 }
 
 async function checkModuleHasChange (modulePath, moduleInfo) {
-
   const status = await checkGitStatus(modulePath)
 
   for (const file of status.files) {
-
     // Se tem outros arquivos para commitar, já marca como has change diretamente
     if (!['package-lock.json', 'package.json'].includes(file.path)) return true
 
     const git = simpleGit(modulePath)
     const diff = await git.diff(['--unified=0', '--no-color', '--no-prefix', 'package.json'])
 
-    const versionChangeRegex = /^(\+|\-)\s+"version":\s*"\d+\.\d+\.\d+",$/m
+    const versionChangeRegex = /^(\+|)\s+"version":\s*"\d+\.\d+\.\d+",$/m
 
     // Verifica se tem outras alterações além da versão
     if (!versionChangeRegex.test(diff)) return true
-
   }
   return false
 }
@@ -73,33 +68,38 @@ async function generateModuleInfo () {
   const modulesInfo = []
 
   // Lê o diretório raiz
-  for (const moduleName of await fs.promises.readdir(modulesDirectory)) {
-
+  for (const moduleName of await readdir(modulesDirectory)) {
     const modulePath = join(modulesDirectory, moduleName)
-    if ((await fs.promises.stat(modulePath)).isDirectory()) {
-
+    if ((await stat(modulePath)).isDirectory()) {
       let moduleInfo
 
-      const packageJson = await loadJson(join(modulePath, 'package.json'))
+      let packageJson
 
-      //////////////////////////////////////////
+      try {
+        const packageJsonPath = join(modulePath, 'package.json')
+        await access(packageJsonPath, constants.R_OK | constants.W_OK)
+        packageJson = await loadJson(packageJsonPath)
+      } catch {
+        continue
+      }
+
+      /// ///////////////////////////////////////
       // Carrega informações
-      //////////////////////////////////////////
+      /// ///////////////////////////////////////
       try {
         moduleInfo = {
           name: packageJson.name,
           directory: moduleName,
-          version: packageJson.version,
+          version: packageJson.version
         }
-
       } catch (error) {
         console.error(`Diretório inválido ${moduleName}: ${error}`)
         continue
       }
 
-      //////////////////////////////////////////
+      /// ///////////////////////////////////////
       // Verifica o status do Git
-      //////////////////////////////////////////
+      /// ///////////////////////////////////////
       try {
         moduleInfo.hasChanges = await checkModuleHasChange(modulePath, moduleInfo)
         modulesInfo.push(moduleInfo)
@@ -111,9 +111,9 @@ async function generateModuleInfo () {
         console.error(`Erro ao verificar o status do Git para ${moduleName}: ${error}`)
       }
 
-      //////////////////////////////////////////
+      /// ///////////////////////////////////////
       // Verfica se é publicável
-      //////////////////////////////////////////
+      /// ///////////////////////////////////////
       const {
         publishable,
         latestReleaseTag
@@ -122,7 +122,6 @@ async function generateModuleInfo () {
       moduleInfo.isPublishable = (publishable === PUBLISHABLE)
       moduleInfo.latestReleaseTag = latestReleaseTag
       moduleInfo.noTag = (publishable === NO_TAG)
-
     }
   }
 
@@ -130,14 +129,11 @@ async function generateModuleInfo () {
 }
 
 function isModuleInstalled (moduleName) {
-
   try {
-
     // Verifica se o módulo está listado como uma dependência no package.json
     return (
-      (projectPackageJson.dependencies && projectPackageJson.dependencies[moduleName] !== undefined)
-      ||
-      (projectPackageJson.devDependencies && projectPackageJson.devDependencies[moduleName] !== undefined)
+      (projectPackageJson.dependencies && projectPackageJson.dependencies[moduleName] !== undefined) ||
+            (projectPackageJson.devDependencies && projectPackageJson.devDependencies[moduleName] !== undefined)
 
     )
   } catch (error) {
@@ -154,29 +150,22 @@ function isModuleInstalled (moduleName) {
 async function checkModuleInstalledVersion (moduleInfo) {
   const command = `npm list ${moduleInfo.name} --json`
 
-  try {
-    const { stdout } = await spawn(command, undefined, true)
-    const result = JSON.parse(stdout)
-    let installedVersion
-    let nestedModules = {}
+  const { stdout } = await spawn(command, undefined, true)
+  const result = JSON.parse(stdout)
+  let installedVersion
+  const nestedModules = {}
 
-    for (const dependency of Object.keys(result.dependencies)) {
-
-      if (dependency === moduleInfo.name) {
-        installedVersion = result.dependencies[dependency].version
-      } else {
-        nestedModules[dependency] = result.dependencies[dependency]
-      }
-
+  for (const dependency of Object.keys(result.dependencies)) {
+    if (dependency === moduleInfo.name) {
+      installedVersion = result.dependencies[dependency].version
+    } else {
+      nestedModules[dependency] = result.dependencies[dependency]
     }
-    return {
-      installedVersion,
-      nestedModules
-    }
-  } catch (e) {
-    throw e
   }
-
+  return {
+    installedVersion,
+    nestedModules
+  }
 }
 
 /**
@@ -192,7 +181,6 @@ async function checkModuleLinked (moduleInfo) {
     const result = JSON.parse(stdout)
     return !!result.dependencies
   } catch (error) {
-
     if (error.code === 1 && error.stdout) {
       return false
     } else {
@@ -203,23 +191,19 @@ async function checkModuleLinked (moduleInfo) {
 
 // Função recursiva para verificar as versões das dependências
 function checkNestedModules (targetModule, nestedModules, targetVersion, path = '') {
-
   let oldVersions = []
 
   for (const nestedModuleName of Object.keys(nestedModules)) {
-
     const nestedModule = nestedModules[nestedModuleName]
     const pathNestedModule = `${path} -> ${nestedModuleName}`
 
     if (nestedModuleName === targetModule) {
-
       if (nestedModule.version !== targetVersion) {
         oldVersions.push({
           path: pathNestedModule,
           version: nestedModule.version
         })
       }
-
     }
 
     if (nestedModule.dependencies) {
@@ -245,11 +229,10 @@ function printTable (modules) {
     chalk.bold('Last Version'),
     chalk.bold('Instaled Version'),
     chalk.bold('Nested dependencies'),
-    chalk.bold('Status'),
+    chalk.bold('Status')
   ])
 
   for (const module of modules) {
-
     const linked = module.link
     const hasChanges = module.hasChanges
     const isPublishable = module.isPublishable
@@ -272,7 +255,7 @@ function printTable (modules) {
       isPublishable ? chalk.red.bold('YES') : 'No',
       module.version,
       oldVersion ? chalk.red.bold(module.instaledVersion) : module.instaledVersion,
-      oldNesteModulesDescription ? oldNesteModulesDescription : 'OK',
+      oldNesteModulesDescription || 'OK',
       warn ? chalk.red.bold('Check Module!!!') : chalk.green.bold('OK')
     ])
 
@@ -286,39 +269,42 @@ function printTable (modules) {
   return statusOk
 }
 
-/////////////////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////////////
 // Carrega modulos analisáveis
-/////////////////////////////////////////////////////////////////
-const moduleInfo = await generateModuleInfo()
+/// //////////////////////////////////////////////////////////////
+try {
+  const moduleInfo = await generateModuleInfo()
 
-/////////////////////////////////////////////////////////////////
-// Verifica quais desses módulos estão instalados
-/////////////////////////////////////////////////////////////////
+  /// //////////////////////////////////////////////////////////////
+  // Verifica quais desses módulos estão instalados
+  /// //////////////////////////////////////////////////////////////
 
-const installedModules = moduleInfo.filter((module) =>
-  isModuleInstalled(module.name)
-)
+  const installedModules = moduleInfo.filter((module) =>
+    isModuleInstalled(module.name)
+  )
 
-/////////////////////////////////////////////////////////////////
-// Verifica versão instalada e se Modulo está linkado
-/////////////////////////////////////////////////////////////////
+  /// //////////////////////////////////////////////////////////////
+  // Verifica versão instalada e se Modulo está linkado
+  /// //////////////////////////////////////////////////////////////
 
-for (const installedModule of installedModules) {
-  const result = await checkModuleInstalledVersion(installedModule)
-  installedModule.instaledVersion = result.installedVersion
-  installedModule.nestedModules = result.nestedModules
+  for (const installedModule of installedModules) {
+    const result = await checkModuleInstalledVersion(installedModule)
+    installedModule.instaledVersion = result.installedVersion
+    installedModule.nestedModules = result.nestedModules
 
-  installedModule.link = await checkModuleLinked(installedModule)
+    installedModule.link = await checkModuleLinked(installedModule)
 
-  installedModule.oldNestedModules = checkNestedModules(installedModule.name, installedModule.nestedModules, installedModule.version)
+    installedModule.oldNestedModules = checkNestedModules(installedModule.name, installedModule.nestedModules, installedModule.version)
+  }
 
-}
+  /// //////////////////////////////////////////////////////////////
+  // Check Nested Deps
+  /// //////////////////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////////////////
-// Check Nested Deps
-/////////////////////////////////////////////////////////////////
-
-if (!printTable(installedModules)) {
+  if (!printTable(installedModules)) {
+    process.exit(1)
+  }
+} catch (error) {
+  console.error(error)
   process.exit(1)
 }
-
